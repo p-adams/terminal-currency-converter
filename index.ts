1eacb4e84c320e178e31418bca528ad2988539fe
Paul Adams <ghusiyy@gmail.com>
2023-02-10 22:34:30 -0800

    pass command-line args to currency converter object

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
-function main() {}
+import CurrencyConverter from "./lib/CurrencyConverter.ts";
+
+function main() {
+  // TODO: validate args input
+  const [amount, from, to] = Deno.args;
+  const currencyConverter = new CurrencyConverter({ amount, from, to });
+  currencyConverter.printConversion();
+}
 
 main();
--- lib/CurrencyConverter.ts
+++ lib/CurrencyConverter.ts
@@ -1,3 +1,33 @@
-export class CurrencyConverterCoordinator {
-  constructor() {}
+interface CurrencyConverter {
+  amount: string;
+  from: string;
+  to: string;
+}
+
+export default class CurrencyConverterCoordinator {
+  #amount: number;
+  #from: string;
+  #to: string;
+  constructor({ amount, from, to }: CurrencyConverter) {
+    this.#amount = +amount;
+    this.#from = from;
+    this.#to = to;
+  }
+
+  public get amount(): number {
+    return this.#amount;
+  }
+
+  public get from(): string {
+    return this.#from;
+  }
+
+  public get to(): string {
+    return this.#to;
+  }
+
+  public printConversion() {
+    const conversion = 0;
+    console.log(`${this.from}:${this.amount} => ${this.to}:${conversion}`);
+  }
 }
