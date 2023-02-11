3bfe15c01ded0ecd7cbbc130dfb42fb7bc17f869
Paul Adams <ghusiyy@gmail.com>
2023-02-10 23:41:40 -0800

    remove TODO

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import CurrencyConverter from "./lib/CurrencyConverter.ts";
 
 function main() {
-  // TODO: validate args input
   const [amount, from, to] = Deno.args;
   const currencyConverter = new CurrencyConverter({ amount, from, to });
   currencyConverter.printConversion();
